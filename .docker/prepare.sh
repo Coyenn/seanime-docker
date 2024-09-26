a5e38391165a120bb43cfadf70f8bfa369da8fa9
Tim Ritter <timrittermail@gmail.com>
2024-09-26 09:25:28 +0200

    Use version when building

--- .docker/prepare.sh
+++ .docker/prepare.sh
@@ -5,4 +5,11 @@ cd "$(dirname "$0")"/../
 
 # Clone the latest version of Seanime
 git clone https://github.com/5rahim/seanime.git
+
+# Checkout the specified release tag
+cd seanime
+git checkout $1
+cd ..
+
+# Move the files to the root of the project
 mv seanime/* .
